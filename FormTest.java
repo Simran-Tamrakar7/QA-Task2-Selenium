77d07f672dc5a28c4f7784703347733c7dd9ef4d
Simrantamrakar7 <simrantamrakar77@gmail.com>
2025-01-24 16:21:46 +0545

    Compiled Code v1

---
+++ FormTest.java
@@ -0,0 +1,43 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FormTest{
+    public static void main(String[] args) {
+        // chrome driver
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver\\chromedriver.exe");
+
+
+        // Initialize
+        WebDriver driver = new ChromeDriver();
+
+        // url navigation
+        driver.get("https://chulo-solutions.github.io/qa-internship/");
+        
+        // form info fill
+        WebElement usernameField = driver.findElement(By.id("username"));
+        usernameField.sendKeys("testuser");
+       
+        WebElement passwordField = driver.findElement(By.id("password"));
+        passwordField.sendKeys("Test@123");
+       
+        WebElement creditcardField = driver.findElement(By.id("creditcard"));
+        creditcardField.sendKeys("4111666622227777");
+       
+        WebElement telephoneField = driver.findElement(By.id("telephone"));
+        telephoneField.sendKeys("(977) 234-2432)");
+
+        WebElement successMessage = driver.findElement(By.id("successMessage"));
+        if (successMessage.isDisplayed()){
+            System.out.println("TestPassed: Form Submission Successful");
+        }
+        else{
+            System.out.println("Test Failed: Form Submission Unsuccessful.");
+        }
+    driver.quit();
+    
+    
+    }
+
+}
